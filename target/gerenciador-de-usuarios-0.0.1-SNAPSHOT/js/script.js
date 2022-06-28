94886fc8482a05152a58fa54e9e225b996894f32
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-06-28 17:53:26 -0300

    target e idea geradas

---
+++ target/gerenciador-de-usuarios-0.0.1-SNAPSHOT/js/script.js
@@ -0,0 +1,13 @@
+function redirecionar(){
+        window.location.href = "telas/painel.html";
+    
+}
+function novoUsuario(){
+    window.location.href = "adicionar.html";
+}
+function voltar(){
+    window.location.href = "painel.html";
+}
+function editarUsuario(){
+    window.location.href = "editar.html";
+}
