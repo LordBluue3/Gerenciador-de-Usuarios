5abfbc0082f46f2801ff75d596a49df64257e125
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-07-08 14:51:10 -0300

    Modificando nome das variaveís

--- src/main/java/com/mikael/model/JavaBeans.java
+++ src/main/java/com/mikael/model/JavaBeans.java
@@ -2,16 +2,11 @@ package com.mikael.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Data;
-
-@Data
 @Getter
 @Setter
 public class JavaBeans {
-	
 	private String id;
-	private String usuario;
+	private String user;
 	private String email;
-	private String senha;
-
+	private String password;
 }
