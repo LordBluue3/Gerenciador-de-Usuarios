6678e550cb0186e763acc907bc2b8c1fa281d9c6
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-07-08 14:50:51 -0300

    Adicionando o método actions e modificando nome de variaveis e métodos

--- src/main/java/com/mikael/model/Dao.java
+++ src/main/java/com/mikael/model/Dao.java
@@ -1,59 +1,96 @@
 package com.mikael.model;
 
+import lombok.SneakyThrows;
 import org.json.simple.JSONObject;
 
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Collections;
+
 import java.util.List;
 
 public class Dao {
+    /*
+     * Qualquer porta entre 0 e 65535.
+     * Seu IPV4.
+     */
+    private String port = "5000";
+    private String server = "192.168.15.7";
 
-    private String porta = "5000"; //Qualquer porta entre 0 e 65535
-    private String servidor = "192.168.15.7"; //O seu IPV4
+    JSONObject json = new JSONObject();
 
-
-    public void conexao(JavaBeans jao){
-        JSONObject json = new JSONObject();
-
-        json.put("usuario",jao.getUsuario());
-        json.put("email",jao.getEmail());
-        json.put("senha",jao.getSenha());
-
-        try{
+    /*
+     * Método que tenta conexão com o Server Socket atrávez do server e porta
+     * e chama o método que irá realizar ações no servidor.
+     */
+    @SneakyThrows
+    public void connection(JavaBeans javaBeans){
             // Tentado conexão ao servidor
-            Socket socket = new Socket(servidor, Integer.parseInt(porta));
+            Socket socket = new Socket(server, Integer.parseInt(port));
             System.out.println("Conectado ao Server Socket!");
-
-            // obter o fluxo de saída do soquete.
+            actions(socket, javaBeans);
+            System.out.println("Fechando o socket.");
+            socket.close();
+    }
+    /*
+     * Método que transforma as váriasveis da class JavaBeans em json,
+     * chama os método enviar mensagem, receber mensagem e receber usuarios.
+     * Essas mensagens são enviadas e recebidas do server socket
+     */
+    public void actions(Socket socket, JavaBeans javaBeans){
+        json.put("user",javaBeans.getUser());
+        json.put("email",javaBeans.getEmail());
+        json.put("password",javaBeans.getPassword());
+        sendMessage(socket);
+        getMessage(socket);
+    }
+    /*
+     *  Método que envia mensagem para o server socket
+     *
+     * obtem o fluxo de saída do soquete.
+     * cria um fluxo de saída de objeto a partir do fluxo de saída para que possamos enviar um objeto através dele.
+     * criando um array.
+     * transformando o json em string e adicionando no array.
+     * Envia ao Socket a mensagem
+     */
+   @SneakyThrows
+    public void sendMessage(Socket socket) {
             OutputStream outputStream = socket.getOutputStream();
-            // cria um fluxo de saída de objeto a partir do fluxo de saída para que possamos enviar um objeto através dele
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-            //fazer um monte de mensagens para enviar.
             List<String> messages = new ArrayList<>();
-            //enviando mensagem em Json ao servidor
             messages.add(new String(json.toJSONString()));
-            //Enviando mensagem ao socket
+
             System.out.println("Enviando mensagem ao ServerSocket: ");
             System.out.println(json);
             System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=");
             System.out.print("Resultado: ");
             objectOutputStream.writeObject(messages);
 
-            //recebendo mensagem do socket
+        }
+        /*
+         * Método que recebe mensagem do servidor e printa no terminal
+         */
+        @SneakyThrows
+        public  void getMessage(Socket socket){
             DataInputStream dus = new DataInputStream(socket.getInputStream());
             String mgs = dus.readUTF();
             System.out.println(mgs);
 
-            //fechando o socket
-            System.out.println("Fechando o socket.");
-            socket.close();
-
-        }catch(IOException e){
-            e.printStackTrace();
         }
+        /*
+         * Método que recebe uma lista com todos os usuários cadastrados no sistema
+         * (Em construção)
+         */
+    /*
+    @SneakyThrows
+        public void receberUsuario(Socket socket){
+                System.out.println("passou aqui");
+                for (var i = 0; i <= 20; i++){
+                    DataInputStream dos = new DataInputStream(socket.getInputStream());
+                     String usr = dos.readUTF();
+                    System.out.println(usr);
+                }
+        }
+        */
     }
 
-
-}
